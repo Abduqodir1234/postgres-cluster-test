ad2af9472ba46d900b9f2ce2ab9383dfd2c69902
Abduqodir <gofurboyevabduqodir@gmail.com>
2025-04-20 16:07:20 +0500

    initial commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Knex } from 'knex';
+import { InjectConnection } from 'nestjs-knex';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(
+    @InjectConnection('master') private readonly masterConnection: Knex,
+    @InjectConnection('slave') private readonly slaveConnection: Knex,
+  ) {}
+  async getHello() {
+    for (let index = 0; index < 1e9; index++) {
+      console.time(`master connection ${index}`);
+      const data = await this.masterConnection.select('*').from('users');
+      console.log(data.length, 'master connection');
+      console.timeEnd(`master connection ${index}`);
+    }
+
     return 'Hello World!';
   }
 }
