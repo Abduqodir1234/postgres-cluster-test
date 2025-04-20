45eac86a99881c82b9c9da854a353752adaecbf4
Abduqodir <gofurboyevabduqodir@gmail.com>
2025-04-20 15:53:10 +0500

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { KnexModule } from 'nestjs-knex';
+import {
+  masterKnexConfigUseFactory,
+  slaveKnexConfigUseFactory,
+} from './shared/knex/knex.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    KnexModule.forRootAsync(
+      {
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) =>
+          masterKnexConfigUseFactory(configService),
+      },
+      'master',
+    ),
+    KnexModule.forRootAsync(
+      {
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) =>
+          slaveKnexConfigUseFactory(configService),
+      },
+      'slave',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
